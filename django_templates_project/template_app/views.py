a5fa3170c8fe79b0338974ac7bb8db1dc77c0c16
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-15 13:59:18 +0300

    templates jinja syntax vol.2

--- django_templates_project/template_app/views.py
+++ django_templates_project/template_app/views.py
@@ -8,7 +8,12 @@ def index(request):
 
 
 def weather_view(request):
-    weather_dict = {"istanbul": "30", "ankara": "26"}
+    weather_dict = {
+        "istanbul": "30",
+        "ankara": "26",
+        "paris": [10, 15, 20, 21],
+        "rome": {"morning": 10, "evening": 15},
+    }
     return render(request, "template_app/weather.html", context=weather_dict)
 
 
