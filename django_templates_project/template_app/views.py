b9d32728e06dbb9f8336d46789e900f0c32b36e9
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-15 16:29:00 +0300

    jinja if

--- django_templates_project/template_app/views.py
+++ django_templates_project/template_app/views.py
@@ -11,8 +11,9 @@ def weather_view(request):
     weather_dict = {
         "istanbul": "30",
         "ankara": "26",
-        "paris": [10, 15, 20, 21],
+        "paris": [3, 15, 16, 5],
         "rome": {"morning": 10, "evening": 15},
+        "user_premium": True
     }
     return render(request, "template_app/weather.html", context=weather_dict)
 
